6724e99c69256d4ff6c2ae3d5caa666f7fdc17b2
ykgautam09 <yateesh.18262@knit.ac.in>
2020-07-06 23:31:56 +0530

    sync

--- app/routes.js
+++ app/routes.js
@@ -5,44 +5,38 @@ const jwt = require('jsonwebtoken');
 const { response } = require('express');
 
 
-<<<<<<< HEAD
-router.post('/',(req,res)=>{
-    const apiToken =jwt.sign({'_id':'apiToken'},'this is secret')
-    console.log(apiToken+'"""""""""""""""""')
-    res.header('APIauth-token',apiToken).send(apiToken)
+router.post('/', (req, res) => {
+    const apiToken = jwt.sign({ '_id': 'apiToken' }, 'this is secret')
+    console.log(apiToken + '"""""""""""""""""')
+    res.header('APIauth-token', apiToken).send(apiToken)
 })
-=======
 
->>>>>>> bebf0425d584a9d377bebdbc6d1c88c08920d5ea
-router.post('/register',(req,res,next)=>{
- /* user registeration for the quiz */
- let controller = require('./controllers/register');
- controller(req,res,next);
+router.post('/register', (req, res, next) => {
+    /* user registeration for the quiz */
+    let controller = require('./controllers/register');
+    controller(req, res, next);
 });
 
-router.post('/createQuiz',(req,res,next)=>{
- /* New table for the quiz would be created here*/
-<<<<<<< HEAD
- auth.verifyUser(req,res,(err,response)=>{
-     res.send('done')
- })
-res.send('done',)
-=======
- let controller = require('./controllers/createQuiz');
- controller(req,res,next);
->>>>>>> bebf0425d584a9d377bebdbc6d1c88c08920d5ea
+router.post('/createQuiz', (req, res, next) => {
+    /* New table for the quiz would be created here*/
+    auth.verifyUser(req, res, (err, response) => {
+        let controller = require('./controllers/createQuiz');
+        controller(req, res, next);
+        res.send('done')
+
+    })
 });
 
-router.post('/newQues',(req,res,next)=>{
-/*New questions for quiz wil be inserted*/
- let controller = require('./controllers/newQues');
- controller(req, res, next);
+router.post('/newQues', (req, res, next) => {
+    /*New questions for quiz wil be inserted*/
+    let controller = require('./controllers/newQues');
+    controller(req, res, next);
 });
 
-router.get('/quiz/:id',(req,res,next)=>{
-/*Fetching the quiz questions*/
- let controller = require('./controllers/getQuiz');
- controller(req, res, next,req.params.id);
+router.get('/quiz/:id', (req, res, next) => {
+    /*Fetching the quiz questions*/
+    let controller = require('./controllers/getQuiz');
+    controller(req, res, next, req.params.id);
 });
 
 
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
-const PORT = process.env.PORT || '4000';
-=======
 var bodyParser = require('body-parser');
 const PORT = process.env.PORT || '3000';
->>>>>>> bebf0425d584a9d377bebdbc6d1c88c08920d5ea
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,5 +11,5 @@ app.use('/',indexrouter);
 
 app.listen(PORT,(err)=>{
  if(err) throw err;
- console.log('Server  at port '+PORT);
+ console.log('Server at port '+PORT);
 });
